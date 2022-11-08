f0f024e869c0de83d944287b3d78e92bc03911e4
wlindstrom55 <75394313+wlindstrom55@users.noreply.github.com>
2022-11-08 03:30:10 -0600

    finished second phase. deletion now works, had to rewrite a decent amount. No editing yet, that might go to phase 3

--- src/components/App.js
+++ src/components/App.js
@@ -6,45 +6,75 @@ import uniqid from "uniqid";
 class App extends React.Component {
   constructor() {
     super(); //not sure we need props at all here
-    this.state = { array: [], value: { text: "", id: uniqid() } };
+    this.state = {
+      // testRightArray: [],
+      // testLeftArray: [],
+      // testArrayLength: "",
+      // testId: "",
+      array: [],
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeItem = this.removeItem.bind(this);
   }
 
   //IMPORTANT: must make our component a 'controlled component' to deal with forms.
-  //that requires an onChange event and a value={this.state.value} property (on the input).
+  //that requires an onChange event and a task={this.state.task} property (on the input).
   handleChange(event) {
     this.setState({
-      value: {
+      task: {
         text: event.target.value,
-        id: this.state.value.id,
+        id: this.state.task.id, //does this change anything? should just set it to what it already was?
       },
-    }); //basically the state of value is stored
+    }); //basically the state of task is stored
     //as state on the App component, instead of in the form (which doesn't work on React b/c)
   }
 
   handleSubmit(event) {
-    //alert('A task was submitted: ' + this.state.array[i]); //won't work. Can't access array like so
-    this.setState({ array: [...this.state.array, this.state.value] }); //non-mutating concat
     event.preventDefault(); //prevent default behavior (refresh)
-    this.setState({ value: "" }); //this resets the input after submit! neat
+    this.setState({
+      array: [...this.state.array, this.state.task], //non-mutating concat
+    });
+    this.setState({
+      //this resets the input after submit!
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+    });
+  }
+
+  removeItem(event) {
+    //we want to remove from state array using event.target.id
+    const targ = parseInt(event.target.id) + 1; //if you try to concatenate event.target.id + 1, you are doing so as a string
+    //so you have to parseInt it.
+    const leftArray = this.state.array.slice(0, event.target.id); //should take from 0 to target
+    const rightArray = this.state.array.slice(targ); //should take everything to the right of index
+    // this.setState({ testRightArray: rightArray });
+    // this.setState({ testLeftArray: leftArray });
+    const newArray = [...leftArray, ...rightArray];
+    this.setState({ array: newArray });
   }
 
   render() {
+    const { array, task } = this.state; //destructuring
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>
-            Create a Task:
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
+          <label htmlFor="taskInput">Create a Task:</label>
+          <input
+            type="text"
+            id="taskInput"
+            value={task.text}
+            onChange={this.handleChange}
+          />
           <input type="submit" value="Submit" />
         </form>
-        <Overview array={this.state.array} />
+        <Overview array={array} itemRemove={this.removeItem} />
       </div>
     );
   }
